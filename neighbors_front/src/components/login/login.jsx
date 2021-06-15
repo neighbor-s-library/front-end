0ff0255d907fa8ea5e7e0475dee0934b0d3041db
bosees <rladuddlf3@naver.com>
2021-06-15 15:43:22 +0900

    도커 구현 성공

--- neighbors_front/src/components/login/login.jsx
+++ neighbors_front/src/components/login/login.jsx
@@ -18,11 +18,21 @@ const Login = () => {
         console.log(login);
     }
 
+    function onSignIn(googleUser) {
+        // var profile = googleUser.getBasicProfile();
+        console.log(googleUser);
+        // console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+        // console.log('Name: ' + profile.getName());
+        // console.log('Image URL: ' + profile.getImageUrl());
+        // console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    }
+
     return (
         <div className={styles.container}>
             <form className={styles.form}>
                 <h2 className={styles.title}>로그인</h2>
                 <div className={styles.inputBox}>
+                <div class="g-signin2" data-onsuccess="onSignIn" onClick={onSignIn}>asd</div>
                 <h2 className={styles.google} onClick={() => {console.log("무야호")}}>구글로 로그인 하기</h2>
                 <hr />
                 <input ref={emailRef} type="text" className={styles.input} placeholder="이메일"/>
